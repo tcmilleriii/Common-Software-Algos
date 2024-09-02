39077fd604cb82a3e5802123934360b29c32f06a
Trey Miller <tcmilleriii@gmail.com>
2024-09-02 13:42:39 -0700

    Cleaning up my binary sort.
    Signed-off-by: Trey Miller <tcmilleriii@gmail.com>

---
+++ binary_search/include/binary_search.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+
+void recursiveBinarySearch(std::vector<int> search_me, int first, int last, int find_me, int& winning_index);
